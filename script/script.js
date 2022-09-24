abf8377bf6f35e25fac31bb8ee5b6ab4a06372a1
Stas Kozak <Kozak.stasan@gmail.com>
2022-09-24 21:54:34 +0300

    feature: implement dropdown menu

--- script/script.js
+++ script/script.js
@@ -1,7 +1,16 @@
+const burger = document.querySelector('.burger');
+const dropdown_menu = document.querySelector('#dropdown-menu');
 (function () {
-  const burger = document.querySelector('.burger');
-  console.log('burger')
   burger.addEventListener('click', () => {
     burger.classList.toggle('burger-active')
+    dropdown_menu.classList.toggle('dropdown-menu--show')
   })
 }());
+
+(function () {
+  const icon_close = document.querySelector('#dropdown-close');
+  icon_close.addEventListener('click', () => {
+    burger.classList.toggle('burger-active')
+    dropdown_menu.classList.toggle('dropdown-menu--show')
+  })
+}())
